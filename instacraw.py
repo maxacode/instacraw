3a0bf16b42b9635f0c3d8445de033ded2da538b7
maxacode <38732378+maxacode@users.noreply.github.com>
2018-05-02 23:25:07 -0500

    error and private.
    More error control and logging. 
    Checks if the user is private.

--- instacraw.py
+++ instacraw.py
@@ -24,6 +24,7 @@ import urllib.request
 import time
 import datetime
 import os
+import sys
 from selenium import webdriver
 
 ####CODE STARTEED####
@@ -52,6 +53,12 @@ picid = ""
 final = ""
 #All insta ID's
 instaIDAll = []
+#Current Data variable
+currentDate = datetime.datetime.now().strftime("%m-%d-%y")
+#Search for if private:
+ifPrivate = "is_private"
+#List of private or non existing users:
+notUser = []
 
 # looks if users.txt exists if not then creates it.
 usrFile = "users.txt"
@@ -75,147 +82,207 @@ driver = webdriver.Firefox(executable_path="geckodriver/geckodriver")
 
 # Loop that runs for every USER in teh file
 for run in range(numusers):
-    try:
-        print("Starting process for user: " + usernames[numofuser])
-        # total pic
-        totalPic = 1
 
-        # Var to show how many PICS Per User
-        int = 0
-        # Array for final result
-        finalUserAll = []
-        # print(usernames[numofuser])
-        # Making full URL with username
-        fullurl = instaurl + usernames[numofuser]
+    print("Starting process for user: " + usernames[numofuser])
 
-        #driver = webdriver.Firefox(executable_path="geckodriver/geckodriver")
-        driver.get(fullurl)
+    # total pic
+    totalPic = 1
 
 
+    # Var to show how many PICS Per User
+    int = 0
+    # Array for final result
+    finalUserAll = []
 
-        counter = 0
-        while counter < 60:
-            ##print("Scrolling Again")
-            ##print("----------------------------------------------------------------------")
+    # Making full URL with username
+    fullurl = instaurl + usernames[numofuser]
 
-            #scrolling this page 3 
-            for x in range(0, 3):
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                time.sleep(delayLoad)
-                newHeight = driver.execute_script("return document.body.scrollHeight")
-
-
-            # putting page source into var
-            data1 = driver.page_source
-
-            # Parsing and adding all to list
-            data2 = re.sub(r"[^\w-]", " ", data1).split()
-            # print(data2)
-            # Getting the count for number of posts for REFERENCE in later loop.
-            ##searchcount = data2.count(search2) + data2.count(search)
-            ##print("Search Count: " + str(searchcount))
-
-            #Var for this data source since new page source is gotten every time it scrolls.
-            instaID = 0
-            ##print("Insta ID: " + str(instaID))
-
-            # Starting loop to find all the URL's on the site.
-            for i, j in enumerate(data2):
-                if j == search2 or j == search:
-
-                    ##print("I: " + str(i))
-                    ##print("instaID: " + str(instaID))
-                    ##print("Total PIC: " + str(totalPic))
-
-                    # Int to have numbers in the results
-                    int = int + 1
-
-                    # Found the INDEX right before the URL value so adding one to get final URL INDEX
-                    instaID = i - 1
-
-                    #Checking if current picID already in list of picID's for this user. IF not then doing whole process.
-                    if data2[instaID] not in instaIDAll:
-
-                        # Making full URL for all the PIC's
-                        picid = "https://instagram.com/p/" + data2[instaID]
-
-                        ##print("pic id: " + str(picid))
-
-                        # Getting website source into variables
-                        datalike = urllib.request.urlopen(picid).read()
-                        # decding to UTF-8
-                        datalike1 = datalike.decode("utf-8")
-                        # Finding keyword stored in LIKESEARCH for the likes INDEX
-                        datalike2 = re.search(likeSearch, datalike1)
-
-                        ##print("datalike2 Done")
-
-                        # Cropping out all the CHARS around the like count.
-                        start = datalike2.start() - 10
-                        end = datalike2.start()
-                        finalLike = datalike1[start:end]
-
-                        ##print("Final Like Done")
-
-                        # Cropping out even more to get exact like ammount
-                        datalike2 = re.search('="', finalLike)
-                        start = datalike2.end()
-                        finalLike3 = finalLike[start:]
-
-                        ##print("final like 3 done")
-
-                        # Making FINAL STRING
-                        final = (str(totalPic) + " | " + str(datetime.datetime.now()) + " | " + usernames[
-                            numofuser] + " | " + finalLike3 + " | " + picid)
-
-                        #Increasing counter for number of posts.
-                        totalPic = totalPic + 1
-
-                        ##print("Final Done")
-
-                        # Appending all links to master file
-                        alllinks.append(picid)
-
-                        # Appeding FINAL to CURRENT USER
-                        finalUserAll.append(final)
-
-                        #Appending instaID to the list so we can compare once it runs again.
-                        instaIDAll.append(data2[instaID])
-
-                        #Reseting the counter cuz we have found some more that are not in list.
-                        counter = 0
-
-                    else:
-                        #If instaID already in list then add to counter and continue
-                        ##print("Already done this one")
-                        i = i + 10
-                        instaID = instaID + 10
-
-                        #If to many already in list then its scrolled to the bottom probably.
-                        counter = counter + 1
-                        ##print("Counter: " + str(counter))
-
-                    #### END OF IF instaID in ALL array.
-                ### END OF IF J IN SEARCH or SEARCH2
-            ### END OF Enumerating i for J in data2
-        ### END OF WHILE
+    #driver = webdriver.Firefox(executable_path="geckodriver/geckodriver")
+    driver.get(fullurl)
 
 
-        # Reseting for current user.
-        int = 0
 
-        # writing to master file and making sure the URL is not already in there.
-        outAllLinks = open("AllLinks.txt", "a")
-        for line in alllinks:
-            if alllinks[int] in open("AllLinks.txt").read():
-                int = int + 1
-            else:
-                outAllLinks.write(line)
-                outAllLinks.write("\n")
-        outAllLinks.close()
+    counter = 0
 
+    while counter < 60:
+        ##print("Scrolling Again")
+        ##print("----------------------------------------------------------------------")
+        print("Total pics so far: " + str(totalPic))
+
+        #scrolling this page 3
+        for x in range(0, 3):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(delayLoad)
+            newHeight = driver.execute_script("return document.body.scrollHeight")
+
+
+        # putting page source into var
+        data1 = driver.page_source
+
+        # Parsing and adding all to list
+        data2 = re.sub(r"[^\w-]", " ", data1).split()
+        #print(data2)
+        # Getting the count for number of posts for REFERENCE in later loop.
+        ##searchcount = data2.count(search2) + data2.count(search)
+        ##print("Search Count: " + str(searchcount))
+
+        #Var for this data source since new page source is gotten every time it scrolls.
+        instaID = 0
+        ##print("Insta ID: " + str(instaID))
+
+        try:
+            if search2 or search in data2: # Starting loop to find all the URL's on the site.
+                ##print("If searcyh2 or search in data2")
+                for i, j in enumerate(data2):
+                    ##print("For i, j in enumerate")
+                    if j == search2 or j == search:
+                        ##print('if J == search ')
+
+                        ##print("I: " + str(i))
+                        ##print("instaID: " + str(instaID))
+                        ##print("Total PIC: " + str(totalPic))
+
+                        # Int to have numbers in the results
+                        int = int + 1
+
+                        # Found the INDEX right before the URL value so adding one to get final URL INDEX
+                        instaID = i - 1
+
+                        #Checking if current picID already in list of picID's for this user. IF not then doing whole process.
+                        if data2[instaID] not in instaIDAll:
+
+                            # Making full URL for all the PIC's
+                            picid = "https://instagram.com/p/" + data2[instaID]
+
+                            #print("pic id: " + str(picid))
+
+                            # Getting website source into variables
+                            datalike = urllib.request.urlopen(picid).read()
+                            # decding to UTF-8
+                            datalike1 = datalike.decode("utf-8")
+                            # Finding keyword stored in LIKESEARCH for the likes INDEX
+                            datalike2 = re.search(likeSearch, datalike1)
+
+                            ##print("datalike2 Done")
+
+                            # Cropping out all the CHARS around the like count.
+                            start = datalike2.start() - 10
+                            end = datalike2.start()
+                            finalLike = datalike1[start:end]
+
+                            ##print("Final Like Done")
+
+                            # Cropping out even more to get exact like ammount
+                            datalike2 = re.search('="', finalLike)
+                            start = datalike2.end()
+                            finalLike3 = finalLike[start:]
+
+                            ##print("final like 3 done")
+
+                            # Making FINAL STRING
+                            final = (str(totalPic) + " | " + str(datetime.datetime.now()) + " | " + usernames[
+                                numofuser] + " | " + finalLike3 + " | " + picid)
+
+                            #Increasing counter for number of posts.
+                            totalPic = totalPic + 1
+
+                            ##print("Final Done")
+
+                            # Appending all links to master file
+                            alllinks.append(picid)
+
+                            # Appeding FINAL to CURRENT USER
+                            finalUserAll.append(final)
+
+                            #Appending instaID to the list so we can compare once it runs again.
+                            instaIDAll.append(data2[instaID])
+
+                            #Reseting the counter cuz we have found some more that are not in list.
+                            counter = 0
+
+                        else:
+                            #If instaID already in list then add to counter and continue
+                            ##print("Already done this one")
+                            i = i + 10
+                            instaID = instaID + 10
+                            #If to many already in list then its scrolled to the bottom probably.
+                            counter = counter + 1
+                            ##print("Counter: " + str(counter))
+
+                        #### END OF IF instaID in ALL array.
+                    ### END OF IF J IN SEARCH or SEARCH2
+                    elif j == ifPrivate:
+                        i = i + 1
+                        if data2[i] == "true":
+                            curError = ("No Such user or Private profile: '" + usernames[numofuser] + "', Delete from list so process can run faster")
+
+                            #Making list of all non existing users.
+                            notUser.append(usernames[numofuser])
+
+                            curError = (str(datetime.datetime.now()) + " | " + curError)
+                            print(curError)
+
+                            # curError.append(curError2)
+                            # Writing error to todays error log
+                            directory2 = "ErrorLOGS/"
+                            if not os.path.exists(directory2):
+                                os.makedirs(directory2)
+                            errorOut = open(directory2 + currentDate + " ErrorLog.txt", "a")
+                            # for line in curError:
+                            errorOut.write(curError)
+                            errorOut.write("\n")
+                            errorOut.close()
+
+                            counter = 61
+
+
+                ### END OF Enumerating i for J in data2
+
+        except Exception as error:
+
+         #   currentDate = datetime.datetime.now().strftime("%m-%d-%y")
+
+            lineError = "Error on line: {}".format(sys.exc_info()[-1].tb_lineno)
+
+            curError = (str(datetime.datetime.now()) + " | " + usernames[numofuser] + " | " + str(picid) + " | " + str(error) + " | " + lineError)
+            print(curError)
+
+            #curError.append(curError2)
+            # Writing error to todays error log
+            directory2 = "ErrorLOGS/"
+            if not os.path.exists(directory2):
+                os.makedirs(directory2)
+            errorOut = open(directory2 + currentDate + " ErrorLog.txt", "a")
+            #for line in curError:
+            errorOut.write(curError)
+            errorOut.write("\n")
+            errorOut.close()
+
+            #Movign iteration back so it can try again.
+            i = i - 50
+            instaID = instaID - 50
+
+        #continue
+
+    ### END OF WHILE
+
+
+    # Reseting for current user.
+    int = 0
+
+    # writing to master file and making sure the URL is not already in there.
+    outAllLinks = open("AllLinks.txt", "a")
+    for line in alllinks:
+        #if alllinks[int] in open("AllLinks.txt").read():
+         #   int = int + 1
+        #else:
+        outAllLinks.write(line)
+        outAllLinks.write("\n")
+    outAllLinks.close()
+
+    #Checking list of non users and making sure we dont create folders/files for them.
+    if usernames[numofuser] not in notUser:
         # writing to users file.
-        currentDate = datetime.datetime.now().strftime("%m-%d-%y")
         filename = "AllData/" + usernames[numofuser] + "/" + currentDate + ".txt"
         directory = "AllData/" + usernames[numofuser]
         if not os.path.exists(directory):
@@ -226,30 +293,25 @@ for run in range(numusers):
             outfinalUserAll.write("\n")
         outfinalUserAll.close()
 
-        # Stopping browser driver for this user.
-        #driver.quit()
+    # Stopping browser driver for this user.
+    #driver.quit()
 
-        # Keep Going with all the USERNAMES
-        numofuser = numofuser + 1
+    # Keep Going with all the USERNAMES
+    numofuser = numofuser + 1
 
     ### TRY AND EXCEPT HERE
 
-    except Exception as error:
-        currentDate = datetime.datetime.now().strftime("%m-%d-%y")
-        print(error)
-        curError = (str(datetime.datetime.now()) + " | " + usernames[numofuser] + " | " + str(error))
-
-        # Writing error to todays error log
-        directory2 = "ErrorLOGS/"
-        if not os.path.exists(directory2):
-            os.makedirs(directory2)
-        errorOut = open(directory2 + currentDate + " ErrorLog.txt", "a")
-        errorOut.write(curError)
-        errorOut.write("\n")
-        errorOut.close()
-	
-        continue
+    #except Exception as error:
+    #currentDate = datetime.datetime.now().strftime("%m-%d-%y")
+    #print(error)
+    #curError2 = (str(datetime.datetime.now()) + " | " + usernames[numofuser] + " | " + str(error))
+    #curError.append(curError2)
+    #continue
 
 driver.quit()
+
+
+
+
 print("All Done, Thanks!")
-#input()
+# input()
