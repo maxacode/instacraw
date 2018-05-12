abc711fef99eb0a96cd67a2a2d0f4db7d585088b
maxacode <38732378+maxacode@users.noreply.github.com>
2018-05-12 17:38:25 -0500

    Update instacraw.py

--- instacraw.py
+++ instacraw.py
@@ -1,12 +1,8 @@
 # Max Derevencha - 417Code
 # 4/24/2018 -
-# From a list of users in a file it gets RECENT 12 PIC URL's and (LIKE's) for those PICs.
-# InstaCrawl Info/Instructions:
+# From a list of users in a file it gets All thier Post URL's and (LIKE's) for those PICs.
 
-# Tested on: Python 3.5-6: Ubuntu, Windows 10.
-
-# What it does:
-# From a list of users in a file it gets RECENT 12 PIC URL's and (LIKE's) for those PICs.
+# Tested on: Python 3.5-6: Ubuntu
 
 
 # 1) Users file File must be in same directory - Named = users.txt - Will autocreate on first run.
@@ -14,9 +10,6 @@
 # 3) Schedule via CHRON/Scheduler
 # 4) Sight back and drink some coffee.
 
-# Additional installations:
-# apt install python-pip
-# pip install -U selenium
 
 # Importin RE for searchign and URLLIP.rfequest to READ URL's
 import re
